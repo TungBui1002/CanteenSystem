60ecf22694d57235a07ae90a350b3eca0583c6e3
TungBui1002 <tungb100203@gmail.com>
2026-03-11 13:13:56 +0700

    add login & role

--- CanteenSystem/App_Start/RouteConfig.cs
+++ CanteenSystem/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace CanteenSystem
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Users", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "MealOrders", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
