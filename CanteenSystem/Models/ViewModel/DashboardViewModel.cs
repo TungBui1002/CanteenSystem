4588c4cc4480f2d2a7b8748e655fa93a402d0ccc
TungBui1002 <tungb100203@gmail.com>
2026-03-21 10:57:36 +0700

    Check Dashboard Ok

---
+++ CanteenSystem/Models/ViewModel/DashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CanteenSystem.Models.ViewModel
+{
+    public class DashboardViewModel
+    {
+        //Top bộ phận báo nhiều nhất
+        public string DepartmentName { get; set; }
+        public int TotalPortions { get; set; }
+
+        // Cho Top món ăn
+        public string Shift { get; set; }
+        public string MealName { get; set; }
+    }
+}
